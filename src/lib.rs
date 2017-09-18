ed9fa7f63779d1667dcd5c8d0e55a81ae0a31a0a
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2017-09-18 20:27:03 +0000

    Create initial ser module

--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,7 @@
 extern crate error_chain;
 
 mod errors;
+mod ser;
 
 pub use errors::{Error, ErrorKind, Result};
+pub use ser::{to_bytes, Serializer};
---
+++ src/ser.rs
@@ -0,0 +1,5 @@
+pub struct Serializer;
+
+pub fn to_bytes() -> Vec<u8> {
+    Vec::new()
+}

0f9f2404aec865f3c4df3f04a860d6138bfc321f
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2017-09-18 20:51:50 +0000

    Add writer field to Serializer
    - Create constructor for Serializer that receives a borrowed writer

--- src/ser.rs
+++ src/ser.rs
@@ -1,4 +1,20 @@
-pub struct Serializer;
+use std::io::Write;
+
+pub struct Serializer<'w, W>
+where
+    W: Write + 'w,
+{
+    writer: &'w W
+}
+
+impl<'w, W> Serializer<'w, W>
+where
+    W: Write + 'w,
+{
+    pub fn new(writer: &'w W) -> Self {
+        Serializer { writer }
+    }
+}
 
 pub fn to_bytes() -> Vec<u8> {
     Vec::new()

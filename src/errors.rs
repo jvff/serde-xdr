71c7db896c6dd17d304960e6b7a6a1f7456f1d95
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2017-09-18 20:39:08 +0000

    Implement ser::Error and de::Error for Error

--- src/errors.rs
+++ src/errors.rs
@@ -1,1 +1,24 @@
-error_chain! {}
+use std::fmt::Display;
+
+use serde::{ser, de};
+
+error_chain! {
+    errors {
+        Custom(message: String) {
+            description("custom error")
+            display("{}", message)
+        }
+    }
+}
+
+impl ser::Error for Error {
+    fn custom<T: Display>(message: T) -> Self {
+        ErrorKind::Custom(message.to_string()).into()
+    }
+}
+
+impl de::Error for Error {
+    fn custom<T: Display>(message: T) -> Self {
+        ErrorKind::Custom(message.to_string()).into()
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 #[macro_use]
 extern crate error_chain;
+extern crate serde;
 
 mod errors;
 mod ser;

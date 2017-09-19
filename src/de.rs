4c35eafb0a032c1ab6edbcf3f7ba8195339e9b22
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2017-09-19 20:40:24 +0000

    Add reader field to Deserializer
    - Implement a constructor to set the field

--- src/de.rs
+++ src/de.rs
@@ -1,3 +1,19 @@
-pub struct Deserializer;
+use byteorder::ReadBytesExt;
+
+pub struct Deserializer<'r, R>
+where
+    R: ReadBytesExt + 'r,
+{
+    reader: &'r mut R,
+}
+
+impl<'r, R> Deserializer<'r, R>
+where
+    R: ReadBytesExt + 'r,
+{
+    pub fn new(reader: &'r mut R) -> Self {
+        Deserializer { reader }
+    }
+}
 
 pub fn from_bytes(_bytes: &[u8]) {}

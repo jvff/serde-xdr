afca7a922673b69fa4d67fb57043d3d754353a46
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2017-09-22 20:51:13 +0000

    Test serialization contents
    - Use OpaqueData instead of Vec<u8> to avoid storing bytes as 4-byte
      integers

--- src/tests.rs
+++ src/tests.rs
@@ -1,6 +1,7 @@
 use std::io::Cursor;
 
 use super::{from_reader, to_bytes};
+use super::OpaqueData;
 
 #[derive(Debug, Deserialize, Eq, PartialEq, Serialize)]
 enum FileType {
@@ -14,16 +15,49 @@ struct File {
     filename: String,
     filetype: FileType,
     owner: String,
-    data: Vec<u8>,
+    data: OpaqueData,
+}
+
+#[test]
+fn serialized_bytes() {
+    let file_contents: Vec<u8> = "(quit)".as_bytes().into();
+
+    let file = File {
+        filename: "sillyprog".to_string(),
+        filetype: FileType::Exec("lisp".to_string()),
+        owner: "john".to_string(),
+        data: file_contents.into(),
+    };
+
+    let bytes = to_bytes(&file).unwrap();
+
+    let expected_bytes = vec![
+        0x00, 0x00, 0x00, 0x09,
+        's' as u8, 'i' as u8, 'l' as u8, 'l' as u8,
+        'y' as u8, 'p' as u8, 'r' as u8, 'o' as u8,
+        'g' as u8, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x02,
+        0x00, 0x00, 0x00, 0x04,
+        'l' as u8, 'i' as u8, 's' as u8, 'p' as u8,
+        0x00, 0x00, 0x00, 0x04,
+        'j' as u8, 'o' as u8, 'h' as u8, 'n' as u8,
+        0x00, 0x00, 0x00, 0x06,
+        '(' as u8, 'q' as u8, 'u' as u8, 'i' as u8,
+        't' as u8, ')' as u8, 0x00, 0x00,
+    ];
+
+    assert_eq!(bytes, expected_bytes);
 }
 
 #[test]
 fn serialize_deserialize() {
+    let file_contents: Vec<u8> = "(quit)".as_bytes().into();
+
     let initial_file = File {
         filename: "sillyprog".to_string(),
         filetype: FileType::Exec("lisp".to_string()),
         owner: "john".to_string(),
-        data: "(quit)".into(),
+        data: file_contents.into(),
     };
 
     let bytes = to_bytes(&initial_file).unwrap();

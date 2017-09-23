9042e55c96eb74ec87120c81a2db391b58551d45
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2017-09-23 10:38:19 +0000

    Order enumeration variants alphabetically

--- src/de/deserializer/tests/value.rs
+++ src/de/deserializer/tests/value.rs
@@ -2,22 +2,22 @@ use ordered_float::OrderedFloat;
 
 #[derive(Debug, Eq, PartialEq)]
 pub enum Value {
-    Nothing,
     Bool(bool),
+    Bytes(Vec<u8>),
+    Double(OrderedFloat<f64>),
+    Enum(usize, Box<Value>),
+    Float(OrderedFloat<f32>),
     Integer8(i8),
     Integer16(i16),
     Integer32(i32),
     Integer64(i64),
+    None,
+    Nothing,
+    Sequence(Vec<Value>),
+    String(String),
+    Unit,
     UnsignedInteger8(u8),
     UnsignedInteger16(u16),
     UnsignedInteger32(u32),
     UnsignedInteger64(u64),
-    Float(OrderedFloat<f32>),
-    Double(OrderedFloat<f64>),
-    String(String),
-    Bytes(Vec<u8>),
-    None,
-    Unit,
-    Sequence(Vec<Value>),
-    Enum(usize, Box<Value>)
 }

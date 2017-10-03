6005de20ceceb89d5e7a035fe76dba91445444df
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2017-10-03 16:17:47 +0000

    Document top-level module

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,67 @@
+//! XDR serialization and deserialization for Serde.
+//!
+//! This crate implements serialization to and deserialization from the
+//! External Data Representation Standard
+//! ([XDR](https://tools.ietf.org/html/rfc1014)) using the
+//! [Serde](https://serde.rs) serialization and deserialization framework.
+//!
+//! Usage is mainly through the helper functions:
+//!
+//! - [`serde_xdr::ser::to_bytes`](fn.to_bytes.html)
+//! - [`serde_xdr::ser::to_writer`](fn.to_writer.html)
+//! - [`serde_xdr::de::from_reader`](fn.from_reader.html)
+//!
+//! # Examples
+//!
+//! ```
+//! extern crate serde_xdr;
+//! #[macro_use]
+//! extern crate serde_derive;
+//!
+//! use std::io::Cursor;
+//!
+//! use serde_xdr::OpaqueData;
+//! use serde_xdr::{from_reader, to_writer};
+//!
+//! #[derive(Debug, Deserialize, Eq, PartialEq, Serialize)]
+//! enum FileType {
+//!     Text,
+//!     Data(String),
+//!     Exec(String),
+//! }
+//!
+//! #[derive(Debug, Deserialize, Eq, PartialEq, Serialize)]
+//! struct File {
+//!     filename: String,
+//!     filetype: FileType,
+//!     owner: String,
+//!     data: OpaqueData,
+//! }
+//!
+//! fn main() {
+//!     let file_contents: Vec<u8> = "(quit)".as_bytes().into();
+//!
+//!     let initial_file = File {
+//!         filename: "sillyprog".to_string(),
+//!         filetype: FileType::Exec("lisp".to_string()),
+//!         owner: "john".to_string(),
+//!         data: file_contents.into(),
+//!     };
+//!
+//!     // Serialize
+//!     let mut bytes = Vec::new();
+//!
+//!     to_writer(&mut bytes, &initial_file).unwrap();
+//!
+//!     // Deserialize
+//!     let mut cursor = Cursor::new(bytes);
+//!
+//!     let recovered_file = from_reader(&mut cursor).unwrap();
+//!
+//!     assert_eq!(initial_file, recovered_file);
+//! }
+//! ```
+
 #![recursion_limit="256"]
 
 extern crate byteorder;

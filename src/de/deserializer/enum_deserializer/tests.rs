5bf6754327fbf6e1fae4dc25bde33d276e972157
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2019-03-29 16:44:14 +0000

    Test for inexistent enum variation

--- src/de/deserializer/enum_deserializer/tests.rs
+++ src/de/deserializer/enum_deserializer/tests.rs
@@ -4,6 +4,7 @@ use serde::Deserializer as SerdeDeserializer;
 
 use super::super::Deserializer;
 use super::super::tests::{Value, Visitor};
+use super::super::super::errors::DeserializationError;
 
 #[test]
 fn deserialize_first_enum_variant() {
@@ -67,4 +68,24 @@ fn deserialize_third_enum_variant() {
 
     assert_eq!(cursor.position(), 8);
     assert_eq!(result, expected_result);
+}
+
+#[test]
+fn deserialize_inexistent_enum_variant() {
+    static VARIANT_NAMES: [&str; 3] = ["bool", "string", "integer"];
+
+    let mut cursor =
+        Cursor::new(vec![0x00, 0x00, 0x00, 0x03, 0xff, 0xff, 0xff, 0xfd]);
+
+    let result = Deserializer::new(&mut cursor)
+        .deserialize_enum("enum", &VARIANT_NAMES, Visitor)
+        .unwrap_err();
+
+    let expected_error = format!(
+        "custom error message: Cant match received variant 3 with possible variants {:?}",
+        VARIANT_NAMES
+    );
+
+    assert_eq!(cursor.position(), 4);
+    assert_eq!(result.to_string(), expected_error);
 }
